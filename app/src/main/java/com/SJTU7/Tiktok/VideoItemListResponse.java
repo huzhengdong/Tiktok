de95b188ba3c2dcce941acc4d90977881a0cebfe
Szy0127 <1799853523@qq.com>
2021-04-30 00:56:14 +0800

    first commit

---
+++ app/src/main/java/com/SJTU7/Tiktok/VideoItemListResponse.java
@@ -0,0 +1,18 @@
+package com.SJTU7.Tiktok;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class VideoItemListResponse {
+    @SerializedName("feeds")
+    public List<com.SJTU7.Tiktok.VideoItem> feeds;
+    @SerializedName("success")
+    public boolean success;
+
+    public VideoItemListResponse()
+    {
+        feeds = null;
+        success = false;
+    }
+}
